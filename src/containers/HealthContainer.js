da41f735777d3afae8682c74282068d06a13696c
johneckert <johnteckert@gmail.com>
2018-05-14 13:20:38 -0400

    added main container

---
+++ src/containers/HealthContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HealthContainer extends Component {
+  render() {
+    return <div>Health Container</div>;
+  }
+}
+
+export default HealthContainer;
