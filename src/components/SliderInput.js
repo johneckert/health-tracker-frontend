12010c9340c8ae9532d33859abbd98084e31b6eb
johneckert <johnteckert@gmail.com>
2018-05-21 22:42:17 -0400

    ficed slider range

--- src/components/SliderInput.js
+++ src/components/SliderInput.js
@@ -32,7 +32,6 @@ const SliderInput = props => {
         <option value="2" label="2" />
         <option value="3" label="3" />
         <option value="4" label="4" />
-        <option value="5" label="5" />
       </datalist>
     </div>
   );
